79eef60babc9d0d2fd6931ba5cff12f2a121e061
Adefisayomi <ariyoadefisayomi@gmail.com>
2026-01-27 19:51:41 -0800

    Done with frontend, form

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-    res.send("Payment gateway running");
-});
+// Serve frontend
+app.use(express.static("public"));
 
 app.listen(3000, () => {
     console.log("Server running on http://localhost:3000");
